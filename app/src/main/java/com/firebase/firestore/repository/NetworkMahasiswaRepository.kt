3d9e0fd5c2405f63de293160fa64103c5ce3093b
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-12 09:29:12 +0700

    Menambah isian fungsi getMahasiswa

--- app/src/main/java/com/firebase/firestore/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/firebase/firestore/repository/NetworkMahasiswaRepository.kt
@@ -67,8 +67,18 @@ class NetworkMahasiswaRepository(
         }
     }
 
-    override suspend fun getMahasiswaByNim() {
-        TODO("Not yet implemented")
+    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> = callbackFlow {
+        val mhsDocument = firestore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener{value, error ->
+                if (value != null){
+                    val mhs = value.toObject(Mahasiswa::class.java) !! //
+                    trySend(mhs)
+                }
+            }
+        awaitClose{
+            mhsDocument.remove()
+        }
     }
 
 
