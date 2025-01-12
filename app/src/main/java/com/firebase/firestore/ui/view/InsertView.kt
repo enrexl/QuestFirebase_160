79850831a84eb70448c16f561b69d4ddbeb9ac19
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-12 10:06:32 +0700

    Membuat fungsi InsertView dalam InsertView.kt

--- app/src/main/java/com/firebase/firestore/ui/view/InsertView.kt
+++ app/src/main/java/com/firebase/firestore/ui/view/InsertView.kt
@@ -1,4 +1,19 @@
 package com.firebase.firestore.ui.view
 
-class InsertView {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.firebase.firestore.ui.viewmodel.InsertUiState
+import com.firebase.firestore.ui.viewmodel.InsertViewModel
+import com.firebase.firestore.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun InsertView(
+    onBack: () -> Unit,
+    onNavigate : () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
+
