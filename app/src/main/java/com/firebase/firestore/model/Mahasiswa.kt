416eace17ad554e5d3a4fb6ffb584887f3dcc486
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-12 11:27:11 +0700

    Mengganti variable jenis kelamin pada data class Mahasiswa

--- app/src/main/java/com/firebase/firestore/model/Mahasiswa.kt
+++ app/src/main/java/com/firebase/firestore/model/Mahasiswa.kt
@@ -4,7 +4,7 @@ data class Mahasiswa(
     val nim: String,
     val nama: String,
     val alamat: String,
-    val jenis_kelamin: String,
+    val jenisKelamin: String,
     val kelas: String,
     val angkatan: String,
 )
