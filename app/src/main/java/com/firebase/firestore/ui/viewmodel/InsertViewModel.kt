395c1eede0c9b5d4db93c1b3114a929f55b65619
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-12 09:47:58 +0700

    Menambah sealed class FormState kedalam InsertViewModel.kt

--- app/src/main/java/com/firebase/firestore/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/firebase/firestore/ui/viewmodel/InsertViewModel.kt
@@ -12,6 +12,13 @@ class InsertViewModel(
 
 }
 
+sealed class FormState{
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
+
 data class  InsertUiState(
     val insertUiState: MahasiswaEvent = MahasiswaEvent(),
     val isEntyValid: FormErrorState = FormErrorState()
