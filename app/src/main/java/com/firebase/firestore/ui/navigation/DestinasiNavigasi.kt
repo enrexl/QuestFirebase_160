087b5049ead60c0da60a11488c4b7eaced09551c
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-12 10:50:54 +0700

    membuat object destinasi home dan insert pada DestinasiNavigasi.kt

--- app/src/main/java/com/firebase/firestore/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/firebase/firestore/ui/navigation/DestinasiNavigasi.kt
@@ -1,4 +1,22 @@
 package com.firebase.firestore.ui.navigation
 
+
+
 interface DestinasiNavigasi {
+    val route: String
+    val titleRes:  String
+}
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route: String
+        = "home"
+    override val titleRes: String
+        = "Home"
+}
+
+object DestinasiInsert : DestinasiNavigasi{
+    override val route: String
+        = "insert"
+    override val titleRes: String
+        = "Insert"
 }
