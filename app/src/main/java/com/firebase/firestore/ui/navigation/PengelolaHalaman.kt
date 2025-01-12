e67b8d3486600be488bffcf1668a5f95badcd4f2
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-12 11:00:27 +0700

    Mengisi fungsi PengelolaHalaman.kt

--- app/src/main/java/com/firebase/firestore/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/firebase/firestore/ui/navigation/PengelolaHalaman.kt
@@ -1,2 +1,39 @@
 package com.firebase.firestore.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.firebase.firestore.ui.view.HomeView
+import com.firebase.firestore.ui.view.InsertView
+import java.lang.reflect.Modifier
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+){
+
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+    ) {
+        composable(DestinasiHome.route) {
+            HomeView(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
+
