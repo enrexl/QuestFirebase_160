c4fa35e2423364032dfabf5fc927b84a15c1e47d
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-12 09:41:19 +0700

    Menambah fungsi MahasiswaEvent kedalam InsertViewModel.kt

--- app/src/main/java/com/firebase/firestore/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/firebase/firestore/ui/viewmodel/InsertViewModel.kt
@@ -12,3 +12,13 @@ class InsertViewModel(
 
 }
 
+
+// Menyimpan input form ke dalam entity
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan,
+)
