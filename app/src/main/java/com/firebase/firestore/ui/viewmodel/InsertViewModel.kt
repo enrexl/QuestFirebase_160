fb6360f5a3f033daf4025a8ba68fc91ef64d05ba
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-12 09:40:59 +0700

    Create InsertViewModel.kt

---
+++ app/src/main/java/com/firebase/firestore/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,14 @@
+package com.firebase.firestore.ui.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.firebase.firestore.model.Mahasiswa
+import com.firebase.firestore.repository.MahasiswaRepository
+
+class InsertViewModel(
+    private val mhs: MahasiswaRepository
+): ViewModel() {
+
+
+}
+
