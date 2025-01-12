ac44c4515553f84e875ff472292c575486231581
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-12 10:32:45 +0700

    Membuat Fungsi InsertBodyMhs pada InsertView.kt

--- app/src/main/java/com/firebase/firestore/ui/view/InsertView.kt
+++ app/src/main/java/com/firebase/firestore/ui/view/InsertView.kt
@@ -7,8 +7,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -21,10 +25,13 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.firebase.firestore.model.Mahasiswa
 import com.firebase.firestore.ui.viewmodel.FormErrorState
+import com.firebase.firestore.ui.viewmodel.FormState
+import com.firebase.firestore.ui.viewmodel.HomeUiState
 import com.firebase.firestore.ui.viewmodel.InsertUiState
 import com.firebase.firestore.ui.viewmodel.InsertViewModel
 import com.firebase.firestore.ui.viewmodel.MahasiswaEvent
 import com.firebase.firestore.ui.viewmodel.PenyediaViewModel
+import java.text.Normalizer
 
 @Composable
 fun InsertView(
@@ -34,6 +41,41 @@ fun InsertView(
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
 
+}
+
+@Composable
+fun InsertBodyMhs(
+    modifier: Modifier = Modifier,
+    onValueChange: (MahasiswaEvent) -> Unit,
+    uiState: InsertUiState,
+    onClick: ()-> Unit,
+    homeUiState: FormState
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormMahasiswa(
+            mahasiswaEvent = uiState.insertUiEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = homeUiState !is FormState.Loading
+        ) {
+            if (homeUiState is FormState.Loading){
+                CircularProgressIndicator(color = Color.Cyan, modifier = Modifier.size(20.dp).padding(end = 8.dp))
+            Text("Loading...")
+            }
+            else
+            {Text("Add")
+        }
+    }
+}
 }
 
 @Composable
